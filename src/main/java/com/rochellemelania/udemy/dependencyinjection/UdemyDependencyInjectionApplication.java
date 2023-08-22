4230f2d7b6d6cf143abc0eb7a475a630dd525e6f
rochelle <rpjuanico@gmail.com>
2023-08-21 17:15:41 -0700

    Initial commit

---
+++ src/main/java/com/rochellemelania/udemy/dependencyinjection/UdemyDependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.rochellemelania.udemy.dependencyinjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyDependencyInjectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemyDependencyInjectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rochellemelania/udemy/dependencyinjection/UdemyDependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rochellemelania.udemy.dependencyinjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyDependencyInjectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
